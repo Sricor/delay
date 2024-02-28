2a98877539071419f80919dd1f405bd3ae977f92
Sricor jo <josricor@outlook.com>
2024-02-28 09:50:55 +0000

    chore: fmt

--- src/manager.rs
+++ src/manager.rs
@@ -1,160 +1,98 @@
-// ===== Task Manager =====
-pub struct TaskManager {
-    tasks: Mutex<HashMap<TaskIdentifier, Arc<Task>>>,
-    sender: mpsc::Sender<Arc<Task>>,
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use tokio::sync::mpsc::{self, Receiver, Sender};
+
+use crate::{
+    error::DelayError,
+    task::{Task, TaskIdentifier},
+};
+
+type DelayResult<T> = Result<T, DelayError>;
+
+pub struct Delay {
+    sender: Sender<Arc<Task>>,
+    inventory: Mutex<HashMap<String, Arc<Task>>>,
 }
 
-impl TaskManager {
+impl Delay {
     // Create a new TaskManager
     pub fn new() -> Self {
-        let (sender, receiver) = mpsc::channel::<Arc<Task>>(32);
-        let tasks = Mutex::new(HashMap::new());
+        let (sender, receiver) = mpsc::channel(32);
+        let inventory = Mutex::new(HashMap::new());
 
-        TaskManager::executor(receiver);
-        TaskManager { tasks, sender }
+        // Self::guard(receiver);
+        Self { inventory, sender }
     }
 
-    // Add a new task to the TaskManager
-    pub fn insert_task(&self, task: Arc<Task>) -> Result<TaskIdentifier, TaskManagerError> {
-        let id = task.id.clone();
-        if self.is_exists_task(&task.id) {
-            return Err(TaskManagerError::TaskAlreadyExists(task.id.clone()));
-        }
+    pub fn insert(&self, task: Task) -> DelayResult<()> {
+        let identifier = task.identifier().clone();
 
-        {
-            let mut tasks = self.tasks.lock()?;
-            tasks.insert(task.id.clone(), task.clone());
+        if let Some(_) = self.get(task.identifier()) {
+            return Err(DelayError::TaskAlreadyExists(identifier));
         };
 
-        if task.valid.load(Ordering::Relaxed) {
-            self.run_task(task);
-        }
-
-        Ok(id)
-    }
-
-    pub fn remove_task(&self, id: &TaskIdentifier) -> Result<(), TaskManagerError> {
-        let _ = self.stop_task(id)?;
-
-        let mut tasks = self.tasks.lock()?;
-        match tasks.remove(id) {
-            Some(_) => Ok(()),
-            None => Err(TaskManagerError::TaskNotExists(id.clone())),
-        }
-    }
-
-    /// Forcibly call a task process, regardless of task validity
-    pub async fn trigger_task(&self, id: &TaskIdentifier) -> Result<(), TaskManagerError> {
-        let task = self.get_task_by_id(id)?;
-        self.sender.send(task).await?;
+        let mut inventory = self.inventory.lock()?;
+        inventory.insert(identifier.clone(), Arc::new(task));
 
         Ok(())
     }
 
-    pub fn stop_task(&self, id: &TaskIdentifier) -> Result<(), TaskManagerError> {
-        let task = self.get_task_by_id(id)?;
-        if task.valid.load(Ordering::Relaxed) {
-            task.valid.store(false, Ordering::Relaxed);
-        } else {
-            return Err(TaskManagerError::TaskAlreadyStop(id.clone()));
-        }
+    pub fn remove(&self, identifier: &TaskIdentifier) -> DelayResult<Task> {
+        if let None = self.get(identifier) {
+            return Err(DelayError::TaskNotExists(identifier.clone()));
+        };
 
-        Ok(())
+        let mut inventory = self.inventory.lock().unwrap();
+        let task = inventory.remove(identifier).unwrap();
+        let result = Arc::into_inner(task).unwrap();
+
+        Ok(result)
     }
 
-    pub fn start_task(&self, id: &TaskIdentifier) -> Result<(), TaskManagerError> {
-        let task = self.get_task_by_id(id)?;
-        if !task.valid.load(Ordering::Relaxed) {
-            task.valid.store(true, Ordering::Relaxed);
-            self.run_task(task);
-        } else {
-            return Err(TaskManagerError::TaskAlreadyRunning(id.clone()));
-        }
+    fn get(&self, identifier: &TaskIdentifier) -> Option<Arc<Task>> {
+        let inventory = self.inventory.lock().unwrap();
+        let task = inventory.get(identifier)?;
 
-        Ok(())
+        Some(task.clone())
     }
 
-    pub fn is_valid_task(&self, id: &TaskIdentifier) -> Result<bool, TaskManagerError> {
-        let task = self.get_task_by_id(id)?;
-
-        Ok(task.valid.load(Ordering::Relaxed))
-    }
-
-    pub fn is_exists_task(&self, id: &TaskIdentifier) -> bool {
-        self.get_task_by_id(id).is_ok()
-    }
-
-    pub fn get_task_by_id(&self, id: &TaskIdentifier) -> Result<Arc<Task>, TaskManagerError> {
-        let tasks = self.tasks.lock()?;
-        let task = tasks.get(id);
-        match task {
-            Some(t) => Ok(t.clone()),
-            None => Err(TaskManagerError::TaskNotExists(id.clone())),
-        }
-    }
-
-    pub fn task_count(&self) -> Result<usize, TaskManagerError> {
-        let count = self.tasks.lock()?.len();
-
-        Ok(count)
-    }
-
-    fn run_task(&self, task: Arc<Task>) {
-        let sender = self.sender.clone();
+    fn executor(task: Arc<Task>) {
         tokio::spawn(async move {
-            if task.running.load(Ordering::Relaxed) {
-                return;
-            }
-
-            task.running.store(true, Ordering::Relaxed);
-            'runner: loop {
-                tokio::time::sleep(task.interval).await;
-
-                // TODO: Max running
-
-                if task.valid.load(Ordering::Relaxed) {
-                    sender.send(task.clone()).await.unwrap();
-                } else {
-                    break 'runner;
-                }
-            }
-
-            task.running.store(false, Ordering::Relaxed);
+            task.sleep().await;
+            task.trigger().await;
         });
     }
 
-    fn executor(mut receiver: mpsc::Receiver<Arc<Task>>) {
-        tokio::spawn(async move {
-            while let Some(task) = receiver.recv().await {
-                tokio::spawn(async move {
-                    // Determine whether the maximum number of runs has been reached
-                    if let Some(limit) = &task.max_run_count {
-                        let max_run_count = limit.load(Ordering::Relaxed);
-                        if task.run_count.load(Ordering::Relaxed) >= max_run_count {
-                            task.valid.store(false, Ordering::Relaxed);
-                            return;
-                        }
-                    }
+    // fn guard(mut receiver: Receiver<Arc<Task>>) {
+    //     tokio::spawn(async move {
+    //         while let Some(task) = receiver.recv().await {
+    //             if task.strong_count() > 2 {
+    //                 continue;
+    //             }
 
-                    let task_result = if let Some(time) = task.timeout.as_ref() {
-                        let result = time::timeout(*time, (task.process)()).await;
-                        match result {
-                            Ok(_) => Ok(()),
-                            Err(_) => Err(TaskError::Timeout),
-                        }
-                    } else {
-                        let _ = (task.process)().await;
-                        Ok(())
-                    };
+    //             Self::executor(task);
+    //         }
+    //     });
+    // }
+}
 
-                    if let Some(callback) = task.callback.as_ref() {
-                        callback(task_result).await;
-                    }
+#[cfg(test)]
+mod tests_delay {
+    use super::*;
 
-                    // add one to the number of run count
-                    task.run_count.fetch_add(1, Ordering::Relaxed);
-                });
-            }
-        });
+    fn simple_delay() -> Delay {
+        Delay::new()
     }
+
+    // fn simple_task() -> Task {
+    //     Task::default()
+    // }
+    // #[test]
+    // fn test_insert() {
+    //     let delay = simple_delay();
+    //     delay.insert().unwrap()
+    // }
 }
