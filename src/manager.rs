7148adb7b4e8ed470989861ddc28fe38a9ad5005
Sricor <josricor@outlook.com>
2024-02-29 22:46:09 +0800

    tests: task running

--- src/manager.rs
+++ src/manager.rs
@@ -23,11 +23,11 @@ impl Delay {
         let (sender, receiver) = mpsc::channel(32);
         let inventory = Mutex::new(HashMap::new());
 
-        // Self::guard(receiver);
+        Self::guard(receiver);
         Self { inventory, sender }
     }
 
-    pub fn insert(&self, task: Task) -> DelayResult<()> {
+    pub fn insert(&self, task: Arc<Task>) -> DelayResult<()> {
         let identifier = task.identifier().clone();
 
         if let Some(_) = self.get(task.identifier()) {
@@ -35,48 +35,36 @@ impl Delay {
         };
 
         let mut inventory = self.inventory.lock()?;
-        inventory.insert(identifier.clone(), Arc::new(task));
+        inventory.insert(identifier.clone(), task);
 
         Ok(())
     }
 
-    pub fn remove(&self, identifier: &TaskIdentifier) -> DelayResult<Task> {
+    pub fn remove(&self, identifier: &TaskIdentifier) -> DelayResult<Arc<Task>> {
         if let None = self.get(identifier) {
             return Err(DelayError::TaskNotExists(identifier.clone()));
         };
 
-        let mut inventory = self.inventory.lock().unwrap();
-        let task = inventory.remove(identifier).unwrap();
-        let result = Arc::into_inner(task).unwrap();
+        let mut inventory = self.inventory.lock()?;
+        let result = inventory.remove(identifier).unwrap();
 
         Ok(result)
     }
 
-    fn get(&self, identifier: &TaskIdentifier) -> Option<Arc<Task>> {
+    pub fn get(&self, identifier: &TaskIdentifier) -> Option<Arc<Task>> {
         let inventory = self.inventory.lock().unwrap();
         let task = inventory.get(identifier)?;
 
         Some(task.clone())
     }
 
-    fn executor(task: Arc<Task>) {
+    fn guard(mut receiver: Receiver<Arc<Task>>) {
         tokio::spawn(async move {
-            task.sleep().await;
-            task.trigger().await;
+            while let Some(task) = receiver.recv().await {
+                task.run().await;
+            }
         });
     }
-
-    // fn guard(mut receiver: Receiver<Arc<Task>>) {
-    //     tokio::spawn(async move {
-    //         while let Some(task) = receiver.recv().await {
-    //             if task.strong_count() > 2 {
-    //                 continue;
-    //             }
-
-    //             Self::executor(task);
-    //         }
-    //     });
-    // }
 }
 
 #[cfg(test)]
